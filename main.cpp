0f777ee7dc1b04a342f5af4c6e48150eb50f56ef
Ramraj Chimouriya <rc02431618@student.ku.edu.np>
2019-11-28 11:03:02 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "linkedlist.h"
-
+#include <iostream>
+using namespace std;
 int main()
 {
     List l;
+    if(l.isEmpty()) cout<<"List is empty"<<endl; 
     l.addToHead(6);
     l.addToHead(5);
     l.addToHead(4);
@@ -11,5 +13,16 @@ int main()
     l.traverse();
     l.removeFromHead();
     l.removeFromHead();
+    l.traverse();
+    l.addToTail(123);
+    l.traverse();
+    l.remove(5);
+    l.traverse();
+    if(l.search(123)) cout<<"123 found"<<endl;
+    l.addToTail(34);
+    l.andToHead(13);
+    Node *pre;
+    l.retrieve(123,pre);
+    l.add(6,pre);
     l.traverse();
 }
